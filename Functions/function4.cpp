c57219d5f78781a33e8b34550f3be1718c72b011
TechTutorialsHub! <techtutorialshubtalks@gmail.com>
2024-11-10 02:39:59 +0000

    11102024

--- Functions/function4.cpp
+++ Functions/function4.cpp
@@ -1,17 +1,17 @@
-//Function (Argument and Return Value)
-#include <iostream>
-using namespace std;
+// //Function (Argument and Return Value)
+// #include <iostream>
+// using namespace std;
 
-int add (int a, int b);
+// int add (int a, int b);
 
-int main() {
-    int result = add(5,3);
-    cout << "Sum: " << result << endl;
-    return 0;
-}
+// int main() {
+//     int result = add(5,3);
+//     cout << "Sum: " << result << endl;
+//     return 0;
+// }
 
-int add (int a, int b){
-    return a+b; //8
+// int add (int a, int b){
+//     return a+b; //8
 
     
-}
+// }
